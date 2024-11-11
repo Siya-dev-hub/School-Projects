00a8e228a0739f52f734516452b8ec6d4c6ad0e0
Siya-dev-hub <visualbasterd@gmail.com>
2024-11-11 11:31:04 +0200

    add save contacts app

---
+++ Save contacts/API/Contactly/Contactly/Data/ContactlyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Contactly.Models.Domain;
+namespace Contactly.Data
+{
+    public class ContactlyDbContext: DbContext 
+    {
+        public ContactlyDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
